7679458ebf2680c1f9b018b313b5153344e09852
Lloyd <stevens.lloyd@Gmail.com>
2023-03-15 11:35:54 +0000

    added connection to DB

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DBCONNECTIONSTRING']
+
+engine = create_engine(db_connection_string, connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    }
+)
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * FROM topic"))
+  result_all = result.all()
+
+  column_names = result.keys()
+
+topic = []
+for row in result.all():
+  topic.append(dict(zip(column_names, row)))
+
+print(topic)
+
+
